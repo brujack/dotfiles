3ce801208418dedb9f388f3108217a0b555ebf3a
Bruce Jackson <bjackson@pobox.com>
2017-01-10 16:15:06 -0500

    First commit

---
+++ setup_mac.sh
@@ -0,0 +1,50 @@
+!/bin/bash
+
+echo "Installing xcode-stuff"
+xcode-select --install
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+echo "Updating homebrew..."
+brew update
+
+echo "Upgrading homebrew"
+brew upgrade
+
+echo "Installing other brew stuff..."
+brew install tree
+brew install wget
+brew install htop-osx
+brew install the_silver_searcher
+brew install ansible
+brew install zsh
+brew install vim
+
+echo "Cleaning up brew"
+brew cleanup
+
+echo "Installing homebrew cask"
+brew install caskroom/cask/brew-cask
+
+echo "Installing Oh My ZSH..."
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Copying dotfiles from Github"
+cd ~
+git clone --recursive git@github.com:brujack/dotfiles.git dotfiles
+
+echo "Copying dotfiles to their home"
+cp -a ~/dotfiles/.zshrc ~/.zshrc
+cp -a ~/dofiles/bruce.zsh-theme ~/.oh-my-zsh/themes/bruce.zsh-theme
+
+echo "Installing Oh My ZSH..."
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Setting ZSH as shell..."
+chsh -s /bin/zsh
