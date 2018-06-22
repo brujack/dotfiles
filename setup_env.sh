83939f0e18db75d2f8bbea98867fe05e46cd77b7
Bruce Jackson <bjackson@pobox.com>
2018-06-22 09:28:53 -0400

    changed docker install to support rancher 2.0 with kubernetes support

--- setup_env.sh
+++ setup_env.sh
@@ -372,7 +372,6 @@ if [ ${LINUX} ]
 then
   sudo -H apt-get update
   sudo -H apt-get install git -y
-  sudo -H apt-get install docker.io -y
   sudo -H apt-get install gcc -y
   sudo -H apt-get install htop -y
   sudo -H apt-get install iotop -y
@@ -386,6 +385,19 @@ then
   sudo add-apt-repository ppa:gophers/archive
   sudo apt-get update
   sudo apt-get install golang-1.10-go -y
+  # for docker setup for rancher kubernetes setup
+  sudo -H apt-get install apt-transport-https -y
+  sudo -H apt-get install ca-certificates -y
+  sudo -H apt-get install curl -y
+  sudo -H apt-get install software-properties-common -y
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo -H apt-key add -
+  sudo -H add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+  sudo -H apt-get update
+  sudo -H apt-get install docker-ce=17.03.2~ce-0~ubuntu-xenial
+
 fi
 
 echo "Installing pip"
