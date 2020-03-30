c45dda34b563507586eae79e57d5bb06808e74b9
Bruce Jackson <bjackson@pobox.com>
2020-03-30 17:09:11 -0400

    added sonarr install for ubuntu

--- setup_env.sh
+++ setup_env.sh
@@ -741,6 +741,20 @@ if [[ ${SETUP} || ${DEVELOPER} ]]; then
       sudo chown root:root /usr/local/bin/vagrant
     fi
 
+    if [[ ${HOSTNAME} == "kube-0" ]]; then
+      echo "Installing Sonarr"
+      sudo apt install gnupg ca-certificates
+      sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+      echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+      sudo apt update
+      sudo apt install mono-devel
+
+      sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0xA236C58F409091A18ACA53CBEBFF6B99D9B78493
+      echo "deb http://apt.sonarr.tv/ master main" | sudo tee /etc/apt/sources.list.d/sonarr.list
+      sudo apt update
+      sudo apt install nzbdrone
+    fi
+
     # install glances cpu monitor
     python3 -m pip install glances
 
