d6071cc2949397e815893116d18bc706ffa603e7
Bruce Jackson <bjackson@pobox.com>
2020-07-06 13:26:13 -0400

    new script to restart FAHClient properly and kill off un respondent processes

---
+++ scripts/restart_fah.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo /etc/init.d/FAHClient stop
+
+for pid in $(ps -ef | grep -i fah | grep -v grep | awk '{print $2}')
+do
+  sudo kill -9 $pid
+done
+
+sudo /etc/init.d/FAHClient start
