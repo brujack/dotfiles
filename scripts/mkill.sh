5760970af27a64d1d36112babd2e9af16357e4a8
Bruce Jackson <bjackson@pobox.com>
2020-07-06 13:30:10 -0400

    updated mkill.sh and using better "{}" in restart_fah.sh

--- scripts/mkill.sh
+++ scripts/mkill.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 pattern=$1
-processes=`ps ax | grep $pattern | grep -v grep | awk '{print $1 }'`
 
-kill -9 ${processes}
+for pid in $(ps -ef | grep -i "${pattern}" | grep -v grep | awk '{print $2}')
+do
+  sudo kill -9 "${pid}"
+done
--- scripts/restart_fah.sh
+++ scripts/restart_fah.sh
@@ -4,7 +4,7 @@ sudo /etc/init.d/FAHClient stop
 
 for pid in $(ps -ef | grep -i fah | grep -v grep | awk '{print $2}')
 do
-  sudo kill -9 $pid
+  sudo kill -9 "${pid}"
 done
 
 sudo /etc/init.d/FAHClient start
