3bdc7d97fa5c2bb1fc63c9ac394727fb6dadf0e5
Bruce Jackson <bjackson@pobox.com>
2020-07-15 10:10:04 -0400

    added in a couple of sleeps

--- scripts/restart_fah.sh
+++ scripts/restart_fah.sh
@@ -2,9 +2,13 @@
 
 sudo /etc/init.d/FAHClient stop
 
+sleep 2
+
 for pid in $(ps -ef | grep -i fah | grep -v grep | awk '{print $2}')
 do
   sudo kill -9 "${pid}"
 done
+
+sleep 2
 
 sudo /etc/init.d/FAHClient start
