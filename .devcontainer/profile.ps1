45d9e697cdc3d7f1d1cb7538ef69f27d559036e4
Bruce Jackson <bjackson@pobox.com>
2021-12-06 16:53:30 -0500

    updated profile.ps1

--- .devcontainer/profile.ps1
+++ .devcontainer/profile.ps1
@@ -1,6 +1,37 @@
+Import-Module posh-git
 Import-Module oh-my-posh
+Import-Module -Name Microsoft.PowerShell.UnixCompleters
+Import-Module -Name Terminal-Icons
+Import-Module -Name AWSPowerShell.NetCore
 
-if($env:LC_TERMINAL -eq "iTerm2") {
-  $ThemeSettings.Options.ConsoleTitle = $false
-}
+# if($env:LC_TERMINAL -eq "iTerm2") {
+#   $ThemeSettings.Options.ConsoleTitle = $false
+# }
 Set-PoshPrompt -Theme agnosterplus
+
+function BackOne {
+  Set-Location ..
+}
+function BackTwo {
+  Set-Location ../..
+}
+function ShowIcons {
+  Get-ChildItem -Path . -Force
+}
+function ShowIdleDisconnect {
+  (Get-APSFleetList -Region us-east-1) + (Get-APSFleetList -Region us-west-2)|where IdleDisconnectTimeoutInSeconds -ne 0 |select name
+}
+function AWSProfilePrd {
+  Set-AWSCredential -ProfileName multiview-prd
+}
+function AWSProfileTest {
+  Set-AWSCredential -ProfileName multiview-test
+}
+
+New-Alias -Name '..' -Value 'BackOne'
+New-Alias -Name '...' -Value 'BackTwo'
+New-Alias -Name 'll' -Value 'ShowIcons'
+
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd:$true
