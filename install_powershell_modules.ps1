2b26369143101a17c387f39622e572d1e1d8815d
Bruce Jackson <bjackson@pobox.com>
2021-12-06 16:53:05 -0500

    updated with a more complete profile.ps1

--- install_powershell_modules.ps1
+++ install_powershell_modules.ps1
@@ -4,4 +4,7 @@ Install-Module -Name AWSPowerShell.NetCore
 Install-Module -Name Az
 Install-Module -Name Az.Blueprint
 Install-Module -Name Microsoft.Graph
+Install-Module -Name Microsoft.PowerShell.UnixCompleters
 Install-Module -Name oh-my-posh
+Install-Module -Name posh-git
+Install-Module -Name Terminal-Icons
