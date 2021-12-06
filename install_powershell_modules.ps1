c552359037ad5c0d571351a7fc8bb1eca89c3bd6
Bruce Jackson <bjackson@pobox.com>
2021-12-06 10:55:49 -0500

    updated docker compose added in powershell module installation

---
+++ install_powershell_modules.ps1
@@ -0,0 +1,6 @@
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
+Install-Module -Name AWS.Tools.Common
+Install-Module -Name Az
+Install-Module -Name Az.Blueprint
+Install-Module -Name Microsoft.Graph
