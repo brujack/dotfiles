724d839ada58ac5c9955870b10787dd7f06dee42
Bruce Jackson <bjackson@pobox.com>
2022-10-18 13:39:55 -0400

    fixed quotes

--- setup_env.sh
+++ setup_env.sh
@@ -999,14 +999,14 @@ if [[ ${SETUP} || ${DEVELOPER} ]]; then
       if [[ ${FOCAL} ]]; then
         echo "Installing Albert"
         curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
-        echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_$(lsb_release -rs)/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+        echo "deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_$(lsb_release -rs)/ /" | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
         sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_$(lsb_release -rs)/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
         sudo -H apt update
         sudo -H apt install albert -y
       elif [[ ${JAMMY} ]]; then
         echo "Installing Albert"
         curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
-        echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_$(lsb_release -rs)/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+        echo "deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_$(lsb_release -rs)/ /" | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
         sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_$(lsb_release -rs)/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
         sudo -H apt update
         sudo -H apt install albert -y
