4451366cf6cf9175e2dad224bc925a6152659102
Bruce Jackson <bjackson@pobox.com>
2023-01-13 10:06:23 -0500

    added in function awsuse to switch environments

--- .devcontainer/.config/.zshrc.d/1_functions.zsh
+++ .devcontainer/.config/.zshrc.d/1_functions.zsh
@@ -43,3 +43,39 @@ function options() {
     echo "\n\nPlugin: $plugin"; grep -r "^function \w*" $PLUGIN_PATH$plugin | awk '{print $2}' | sed 's/()//'| tr '\n' ', '; grep -r "^alias" $PLUGIN_PATH$plugin | awk '{print $2}' | sed 's/=.*//' |  tr '\n' ', '
   done
 }
+
+function findStringInFile() {
+  if [ -z "$1" ]; then
+    echo "No file supplied"
+  else
+    if [ -z "$2" ]; then
+      echo "No string supplied"
+    else
+      if grep -q "$2" $1; then
+        echo "Found $2 in $1"
+        return 0
+      else
+        echo "Did not find $2 in $1"
+        return 1
+      fi
+    fi
+  fi
+}
+
+function awsuse() {
+  if [ -z "$1" ]; then
+    echo "No environment supplied"
+  else
+    if findStringInFile ~/.aws/config $1; then
+      export AWS_PROFILE=${1}
+      echo "AWS command line environment set to [${1}]"
+      aws sso logout
+      aws sso login
+    else
+      echo "AWS profile [${1}] not found."
+      echo "Please choose from an existing profile:"
+      grep "\[profile" ~/.aws/config
+      echo "Or create a new one."
+    fi
+  fi
+}
