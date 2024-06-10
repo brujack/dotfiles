57b3b84ddfbb38817e974f7e6d675c867d5f2ea0
Bruce Jackson <bjackson@pobox.com>
2024-06-10 12:42:11 -0400

    updated docker install for 24.04

--- setup_env.sh
+++ setup_env.sh
@@ -1315,11 +1315,11 @@ if [[ -n ${SETUP} ]] || [[ -n ${DEVELOPER} ]]; then
       if [[ -f /etc/apt/keyrings/docker.gpg ]]; then
         sudo rm -f /etc/apt/keyrings/docker.gpg
       fi
-      curl -fsSL http://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-      sudo chmod a+r /etc/apt/keyrings/docker.gpg
+      sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+      sudo chmod a+r /etc/apt/keyrings/docker.asc
       echo \
-      "deb [arch='$(dpkg --print-architecture)' signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      '$(. /etc/os-release && echo "$VERSION_CODENAME")' stable" | \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
       sudo -H apt update
       sudo -H apt install docker-ce -y
