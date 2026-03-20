7bea1df5d0258399ea589cf52ed91c80b8474702
Bruce Jackson <bjackson@pobox.com>
2026-03-20 12:00:49 -0400

    added some better math packages for faster python calculations

--- setup_env.sh
+++ setup_env.sh
@@ -2249,7 +2249,7 @@ if [[ -n ${DEVELOPER} ]] || [[ -n ${ANSIBLE} ]]; then
       pyenv virtualenv "${PYTHON_VER}" ansible
       pyenv activate ansible
       printf "Installing Ansible dependencies...\\n"
-      python -m pip install ansible ansible-lint certbot certbot-dns-cloudflare boto3 docker jmespath mpmath netaddr pylint psutil bpytop HttpPy j2cli wheel shell-gpt
+      python -m pip install ansible ansible-lint certbot certbot-dns-cloudflare boto3 docker gmpy2 jmespath mpmath netaddr pylint psutil bpytop HttpPy j2cli wheel shell-gpt
     fi
   fi
 
